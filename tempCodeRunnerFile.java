ce474be30489064c57c8fa5c9b2154d6ee43014b
surbhipw <bhongadesurbhi@gmail.com>
2023-07-29 18:40:01 +0530

    GusserGame

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+   int guessingNumber(){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Player Kindly Guess The Number");
+    guessNum=scan.nextInt();
+    return guessNum;
+    }
