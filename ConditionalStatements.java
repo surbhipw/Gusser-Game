1266950714d03ca315a8276b00730cc7d177517c
surbhipw <bhongadesurbhi@gmail.com>
2023-07-29 19:17:02 +0530

    Conditinals concept

---
+++ ConditionalStatements.java
@@ -0,0 +1,51 @@
+public class ConditionalStatements {
+    public static void main(String args[]){
+        System.out.println("RadheRadhe");
+
+        // // Conditional Statement [if]
+        // int age = 18;
+        // if(age>=18){
+        //     System.out.println("you can vote now");
+
+        // }
+
+
+        // //Conditional Statement [if else]
+        // int age = 12;
+        // if(age>=18){
+        //     System.out.println("you can vote now");
+        // }else{
+        //     System.out.println("you can't vote");
+        // }
+
+
+        // // Conditional Statement [if else-if ]
+        // int age =40 ;
+        // if(age>18){
+        //     System.out.println("you can vote now");
+        // }else if(age>=25){
+        //     System.out.println("you are a good voter");
+        // }else if(age>=40){
+        //     System.out.println("you are the experience voter");
+        // }
+        // else{
+        //     System.out.println("you can't vote");
+        // }
+
+
+
+
+        int marks =55;
+        if(marks<33){
+            System.out.println("You are fail");
+        }else if(marks>60){
+            System.out.println("you are pass with first class");
+        }else if(marks>40){
+            System.out.println("you are pass");
+        }
+        else{
+            System.out.println("do better next time");
+        }
+    }
+        
+}
